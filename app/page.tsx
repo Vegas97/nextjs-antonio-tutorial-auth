b54eb0dd279d5c167cf5be48148d4962d9c2180e
Diego Palo <dp0997@gmail.com>
2024-03-22 09:48:14 +0800

    add buttons + add custom variant super easy !!

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <p className="font-semibold text-green-500">Hello Auth!</p>;
+  return (
+    <div>
+      <p className="font-semibold text-green-500">Hello Auth!</p>
+      <Button size="lg" variant="secondary">
+        Click me
+      </Button>
+      <Button size="lg" variant="custom">
+        Click me
+      </Button>
+    </div>
+  );
 }
