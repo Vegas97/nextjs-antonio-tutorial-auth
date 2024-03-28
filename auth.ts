20a615c677efc55ce833d07c25a14ef1f0c6af8b
Diego Palo <dp0997@gmail.com>
2024-03-28 22:56:05 +0800

    learn and implement how to extend session data from jwt token + UNDERSTOOD CALLBACKS!!

--- auth.ts
+++ auth.ts
@@ -2,6 +2,13 @@ import NextAuth from "next-auth";
 import authConfig from "@/auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/db";
+import { UserRole } from "@prisma/client";
+
+declare module "next-auth" {
+  interface User {
+    role: UserRole;
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +16,76 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async signIn({ credentials, account, profile, user, email }) {
+      console.log({
+        fromSignIn: {
+          credentials,
+          account,
+          profile,
+          email,
+          user,
+        },
+      });
+
+      return true;
+    },
+    async jwt({ token, user, session, profile, account }) {
+      // if user exist, means we are on signIn, we add the user role to the token
+      if (user && user.role) {
+        token.role = user.role;
+      }
+
+      // option2. calling db each time
+
+      // // if sub is not set, return the token as is (we do nothing)
+      // if (!token.sub) return token;
+
+      // // we get the user from db
+      // const existingUser = await getUserById(token.sub);
+
+      // // if user does not exist, return the token as is (we do nothing)
+      // if (!existingUser) return token;
+
+      // // if user exist we add the user role to the token
+      // token.role = existingUser.role;
+
+      console.log({
+        fromJWT: {
+          token,
+          user,
+          session,
+          profile,
+          account,
+        },
+      });
+
+      return token;
+    },
+    async session({ token, session, user, newSession }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        // Conversion from string to enum
+        // session.user.role = UserRole[token.role as keyof typeof UserRole]; // option 1.
+        session.user.role = token.role as UserRole;
+      }
+
+      console.log({
+        fromSession: {
+          token,
+          session,
+          sessionUser: session.user,
+          newSession,
+          user,
+        },
+      });
+
+      return session;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
