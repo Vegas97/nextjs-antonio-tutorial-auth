0be2d991e6565357275bf9caef911d869413c058
Diego Palo <dp0997@gmail.com>
2024-03-28 10:45:17 +0800

    add middleware routes protection PURE LOVE!

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,49 @@
-import authConfig from "./auth.config";
-import NextAuth from "next-auth";
+import authConfig from "@/auth.config";
+import NextAuth, { Session } from "next-auth";
+import {
+  apiAuthPrefix,
+  authRoutes,
+  DEFAULT_LOGIN_REDIRECT,
+  publicRoutes,
+} from "@/routes";
+import { NextRequest } from "next/server";
 
 export const { auth } = NextAuth(authConfig);
 
-// middleware works on the edge.
-// but prisma don't support it, we can't use prism adapter in middleware
-// BUT there is a solution.
+export default auth(
+  (req: NextRequest & { auth: Session | null }): Response | void => {
+    const { nextUrl } = req;
+    const isLoggedIn = !!req.auth;
 
-export default auth((req) => {
-  const route = req.nextUrl.pathname;
-  const isLoggedIn = !!req.auth;
+    const info = {
+      route: nextUrl.pathname,
+      isLoggedIn: isLoggedIn,
+    };
 
-  const info = {
-    route: route,
-    isLoggedIn: isLoggedIn,
-  };
+    console.log("middleware ", info);
 
-  console.log("middleware ", info);
-});
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+    if (isApiAuthRoute) {
+      return;
+    }
+
+    if (isAuthRoute) {
+      if (isLoggedIn) {
+        return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+      }
+      return;
+    }
+
+    if (!isLoggedIn && !isPublicRoute) {
+      return Response.redirect(new URL("/auth/login", nextUrl));
+    }
+
+    return;
+  },
+);
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
