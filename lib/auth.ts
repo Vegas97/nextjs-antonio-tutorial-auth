2e340601294b71d18cfa24eea8d9f9c0d26af40f
Diego Palo <dp0997@gmail.com>
2024-04-11 09:22:24 +0800

    style admin page + add api route/ server action to it protected by roleGate

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,3 +5,10 @@ export const currentUser = async () => {
 
   return session?.user;
 };
+
+export const currentRole = async () => {
+  const session = await auth();
+
+  // TODO: why .role is not suggested by the IDE ??
+  return session?.user?.role;
+};
